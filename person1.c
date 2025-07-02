23e99d050246838fee2588a256d8ca646be3c282
SaziaRahman <saziarahman2000@gmail.com>
2025-07-02 08:49:48 +0600

    C programming work

--- person1.c
+++ person1.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
 typedef char *string;
 
-bool compare(string a[], string b[])
-{
-    int n = sizeof(a)/sizeof(a[0]);
-    for(int i = 0; i<n; i++)
-    {
-    if (a[i] != b[i])
-        return false;
-    }
-    return true;
-}
-
-
 int main(void)
 {
-    string t1[1], t2[1];
+    string t1, t2;
     printf("Type 1st person's name:");
-    scanf("%s", t1);
+    scanf("%s", &t1);
     printf("Type 2nd person's name:");
-    scanf("%s", t2);
+    scanf("%s", &t2);
     
-    if (compare(t1, t2))
+    if (t1 == t2)
     {
-        printf("Hello, %s\n", t1);
+        printf("Hello, %s\n", &t1);
     }
     else
     {
