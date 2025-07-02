8ea139bc1f58ea2e38fc54ccb2aae0056e9f89a5
SaziaRahman <saziarahman2000@gmail.com>
2025-07-02 20:00:32 +0600

    C programming

--- person1.c
+++ person1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 typedef char *string;
 
+bool compare(string *a, string *b)
+{
+    string a1 = *a, b1 = *b;
+    if(a1!=b1)
+    {
+        return false;
+    }
+    return true;
+}
+
 int main(void)
 {
     string t1, t2;
@@ -8,8 +18,7 @@ int main(void)
     scanf("%s", &t1);
     printf("Type 2nd person's name:");
     scanf("%s", &t2);
-    
-    if (t1 == t2)
+    if (compare(&t1, &t2))
     {
         printf("Hello, %s\n", &t1);
     }
