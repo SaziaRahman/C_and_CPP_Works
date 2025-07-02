738b6a1a9bb438bd693fc993c675e6b86f42c45b
SaziaRahman <saziarahman2000@gmail.com>
2025-07-02 20:27:13 +0600

    C programming

--- person1.c
+++ person1.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef char *string;
 
-bool compare(string *a, string *b)
+bool compare(char *a, char *b)
 {
-    string a1 = *a, b1 = *b;
+    int n = sizeof(a)/sizeof(a[0]);
+    for(int i = 0; i < n; i++)
+    {
+    char a1 = a[i], b1 = b[i];
     if(a1!=b1)
     {
         return false;
+    }
     }
     return true;
 }
 
 int main(void)
 {
-    string t1, t2;
+    string t1 = (string) malloc(100), t2 = (string) malloc(100); //string t1[100], t2[100]
+
     printf("Type 1st person's name:");
-    scanf("%s", &t1);
+    scanf("%s", t1);
     printf("Type 2nd person's name:");
-    scanf("%s", &t2);
-    if (compare(&t1, &t2))
+    scanf("%s", t2);
+    if (compare(t1, t2))
     {
-        printf("Hello, %s\n", &t1);
+        printf("Hello, %s\n", t1);
     }
     else
     {
         printf("Hello, world\n");
     }
+    free(t1);
+    free(t2);
     return 0;
 }
